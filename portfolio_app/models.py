be1154be054da3bfe75d5ef9ca288a318f8e17fd
Yusuf Çelebi <ultraslancelebi@gmail.com>
2025-03-11 05:40:13 +0300

    style: Designed content section like a vintage notebook  feat: Added dark mode support

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    profession= models.CharField(max_length=50)
+    bio= models.TextField(max_length=1000)
+    github_url= models.URLField(max_length=200)
+    instagram_url= models.URLField(max_length=200)
+    linkedin_url=models.URLField(max_length=200)
+    profile_picture=models.ImageField(upload_to="images/",null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
